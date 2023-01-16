cbd3ff2f53de338ded5e77058ade5d96889edcb3
SonicXmaximumHit <60919711+SoniXmaximumHit@users.noreply.github.com>
2023-01-16 15:45:57 +0500

    Update user_text_util.php
    2) Добавила функцию открытия и считывания файлов из папки texts/

--- user_text_util.php
+++ user_text_util.php
@@ -27,8 +27,40 @@ function reat_csv(){
 }
 
 
+function read_texts()
+{
+    /*     Открывает папку texts/ и считывает 
+    вложенные строки в лист массива */
+    global $arr;
+    reat_csv();
+    echo "<br>\n";
+    $a = [];
+    $dir = './texts/';
+    if ($handle = opendir($dir)) {
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry != "." && $entry != "..") {
+                $filename1 = $dir . $entry;
+                $ffile3 = fopen($filename1, 'r');
+                $contens = fread($ffile3, filesize($filename1));
+                fclose($ffile3);
+                $a+=[$entry=>$contens];
+            }            
+        }
+        closedir($handle);
+    }
+    // var_dump($a);
+    for ($i = 1; $i <= count($arr); $i++) {
+        $b = [];
+        foreach ($a as $key => $value) {
+            $mykey = explode("-", $key)[0];
+            if ($mykey==$i ){
+                array_push($b, $value);
+            }
+        }
+        $arr[$i-1]+=['texts'=>$b];
+    }
+}
 
-reat_csv();
-// print_r($arr);
-echo '<hr>';
-var_dump($arr);
+
+read_texts();
+print_r($arr);
