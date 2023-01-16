ed50e499cfda17c0bdd3fec575cbb9ffacccafaf
SonicXmaximumHit <60919711+SoniXmaximumHit@users.noreply.github.com>
2023-01-16 19:10:28 +0500

    Update user_text_util.php

--- user_text_util.php
+++ user_text_util.php
@@ -1,40 +1,36 @@
-<pre>
+<!-- <pre> -->
 <?php 
-    echo "Hello,  kek <br>\n ";
-
-echo '<pre>';
-
 function reat_csv(){
-    // Открывает .csv файл и считывает 
-    // id(int) и name(str) построчно
-    // в лист словаря
-    // CSV файл в массив:
+/*     Открывает .csv файл и считывает 
+    id(int) и name(str) построчно
+    в лист массива
+    CSV файл в массив: */
     global $arr;$arr=[];
     //$f_csv= 'people.csv'
-    $lines = file('people.csv');    
+    $lines = file('ab.csv');    
     foreach($lines as $line)
     {   
         $arr_i=[];
-        echo($line);        
+        // echo($line);        
         $index_i=explode(";", $line)[0];
         $name_i=explode(";", $line)[1];
-        $arr_i+=['index'=>$index_i];
+        $arr_i+=['namber'=>$index_i];
         $arr_i+=['name'=>$name_i];
         array_push($arr, $arr_i);
         // var_dump($arr);
-        echo '<hr>';
+        // echo '<hr>';
     }
 }
-
 
 function read_texts()
 {
     /*     Открывает папку texts/ и считывает 
     вложенные строки в лист массива */
     global $arr;
+    global $aa;
     reat_csv();
     echo "<br>\n";
-    $a = [];
+    $aa = [];
     $dir = './texts/';
     if ($handle = opendir($dir)) {
         while (false !== ($entry = readdir($handle))) {
@@ -43,25 +39,26 @@ function read_texts()
                 $ffile3 = fopen($filename1, 'r');
                 $contens = fread($ffile3, filesize($filename1));
                 fclose($ffile3);
-                $a+=[$entry=>$contens];
+                $aa+=[$entry=>$contens];
             }            
         }
         closedir($handle);
     }
-    // var_dump($a);
     for ($i = 1; $i <= count($arr); $i++) {
         $b = [];
-        foreach ($a as $key => $value) {
+        foreach ($aa as $key => $value) {
             $mykey = explode("-", $key)[0];
             if ($mykey==$i ){
                 array_push($b, $value);
             }
-        }
+        }        
         $arr[$i-1]+=['texts'=>$b];
     }
 }
 
 
+
+// print_r($arr);
 function countAverageLineCount()
 {
     /* Функция находит и выводит среднее количество 
@@ -69,7 +66,6 @@ function countAverageLineCount()
     global $arr;
     read_texts();
     for ($i = 1; $i <= count($arr); $i++) {
-
         foreach ($arr[$i - 1] as $key => $value) {
             $c_str = 0;
             $mykey = $key;
@@ -77,8 +73,7 @@ function countAverageLineCount()
                 // var_dump($value);
                 foreach ($value as $v) {
                     // echo '<br>Количество строк' . count(explode("\n", $v)) . '<br>в ' . $v;
-                    $c_str += count(explode("\n", $v));
-                    
+                    $c_str += count(explode("\n", $v));                    
                 }
                 // echo count($value);
                 $arr[$i - 1] += ['count_line' => round($c_str/count($value))];
@@ -88,7 +83,7 @@ function countAverageLineCount()
     }
 }
 function output_texts(){
-/*     Открывает папку output_texts/ и добавляет 
+/*     Открывает папку output/ и добавляет 
     туда изменённые файлы из папки texts/ */
     global $aa; 
     $dir = './output/';
@@ -106,7 +101,6 @@ function replaceDates()
     global $arr;
     read_texts();
     for ($i = 1; $i <= count($arr); $i++) {
-
         foreach ($arr[$i - 1] as $key => $value) {
             $c_str = 0;
             $mykey = $key;
@@ -115,15 +109,16 @@ function replaceDates()
                 foreach ($value as $v) {
                     // echo '<br>Количество строк' . count(explode("\n", $v)) . '<br>в ' . $v;
                     $c_str += substr_count( $v,'/');
-//                     echo $v;
+                    // echo $v;
                 }
                 // echo count($value);
                 $arr[$i - 1] += ['count_replace' => $c_str];
             }
-
         }
     }
+    output_texts() ;
 }
+
 function print_symbol($x,$symbol,$y){
     /* Форма строки (имя,символ,число) */
     echo $x.$symbol.$y;
@@ -144,7 +139,6 @@ function print_full($s)
             if ($mykey == 'count_replace') {
                 $b = $value;
                 // echo $b;
-                var_dump($b);
             }
             if ($mykey == 'count_line') {
                 $c = $value;
@@ -169,6 +163,7 @@ function semicolon(){
     print_full(';');
 }
 
-countAverageLineCount();
-comma();
-var_dump($arr);
+// replaceDates();
+// countAverageLineCount();
+// comma();
+// semicolon();
