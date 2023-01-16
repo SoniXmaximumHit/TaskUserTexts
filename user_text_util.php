2307a77c1788f57522ab15d7e33327f2628d03f4
SonicXmaximumHit <60919711+SoniXmaximumHit@users.noreply.github.com>
2023-01-16 11:13:00 +0500

    Update user_text_util.php

--- user_text_util.php
+++ user_text_util.php
@@ -1,30 +1,34 @@
+<pre>
 <?php 
     echo "Hello,  kek <br>\n ";
 
 echo '<pre>';
-if (($file =fopen(filename:'ab.csv',mode:'r'))!==false){
-    while (($data=fgetcsv($file,length:1000))!==false){
-        // print_r($data);
-        $out = '';
-        $list = [];
-        $my_arr = [];
-        for ($i = 0; $i < count($data);$i++){
-            $out .= $data[$i] . " "; 
-        }
-        echo $out;
-        // echo $my_arr;
+
+function reat_csv(){
+    // Открывает .csv файл и считывает 
+    // id(int) и name(str) построчно
+    // в лист словаря
+    // CSV файл в массив:
+    global $arr;$arr=[];
+    //$f_csv= 'people.csv'
+    $lines = file('ab.csv');    
+    foreach($lines as $line)
+    {   
+        $arr_i=[];
+        echo($line);        
+        $index_i=explode(";", $line)[0];
+        $name_i=explode(";", $line)[1];
+        $arr_i+=['index'=>$index_i];
+        $arr_i+=['name'=>$name_i];
+        array_push($arr, $arr_i);
+        // var_dump($arr);
         echo '<hr>';
     }
-    fclose($file);
 }
 
-// CSV файл в массив:
-$ref=[];
-if (($file =fopen(filename:'ab.csv',mode:'r'))!==false){
-    while (($data=fgetcsv($file,length:1000))!==false){
-        $ref[]=$data;
-    }
-    fclose($file);
-}
-// var_dump($ref);
-print_r($ref);
+
+
+reat_csv();
+// print_r($arr);
+echo '<hr>';
+var_dump($arr);
